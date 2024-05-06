6f0c97c5c78bb7b1ceceaa0db26ac1c47855d8a1
vizucode <havisikkubaru@gmail.com>
2024-05-06 13:54:01 +0700

    feat

--- main.go
+++ main.go
@@ -6,15 +6,33 @@ import (
 )
 
 func main() {
-	var base float32
-	var height float32
 
-	_, err := fmt.Scan(&base, &height)
+	var (
+		num1 uint
+		num2 uint
+		num3 uint
+		num4 uint
+		num5 uint
+		num6 uint
+		num7 uint
+		num8 uint
+		num9 uint
+	)
+
+	_, err := fmt.Scanln(&num1, &num2, &num3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fmt.Scanln(&num4, &num5, &num6)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fmt.Scanln(&num7, &num8, &num9)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 1/5 * base  * height
-	var area = 0.5 * base * height
-	fmt.Printf("%.2f", area) // 7.5
+	fmt.Println(num1, num4, num7)
+	fmt.Println(num2, num5, num8)
+	fmt.Println(num3, num6, num9)
 }
