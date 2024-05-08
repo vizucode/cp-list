eb4d55594c73bfe62bf48156b0c1e0ce7a689721
vizucode <havisikkubaru@gmail.com>
2024-05-08 12:01:45 +0700

    feat

--- main.go
+++ main.go
@@ -2,15 +2,16 @@
 
    var N = 5
 
-   loops: i; 1 to N (++) = 1,2,3,4,5
-       loops: space; N to i (--) = 5, 4, 3, 2, 1
-       loops: star; 1 to i (++) = 1, 2, 3, 4, 5
+   var num // 0,2,3,4,5,6,7,8,9,0,1,2,3,4
 
-   ####*
-   ###**
-   ##***
-   #****
-   *****
+   i = 1 => 0,1,2,3,4,5
+       j = 1 to eq i => 0,[0,1],[0,1,2],[0,1,2,3],[0,1,2,3,4],[0,1,2,3,4,5]
+       num++
+       if num > 9 {
+           num = 0
+       }
+
+       print(num)
 
 */
 
@@ -19,16 +20,18 @@ package main
 import "fmt"
 
 func main() {
+
 	var N int
 	fmt.Scan(&N)
+	var num int
 
-	for i := 1; i <= N; i++ {
-		for space := N; space > i; space-- {
-			fmt.Print(" ")
-		}
-
-		for star := 1; star <= i; star++ {
-			fmt.Print("*")
+	for i := 0; i < N; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Print(num)
+			num++
+			if num > 9 {
+				num = 0
+			}
 		}
 		fmt.Println("")
 	}
