bcf3222b65bed403ad78a4c8da62d153536c2e71
vizucode <havisikkubaru@gmail.com>
2024-05-09 10:05:57 +0700

    feat

--- main.go
+++ main.go
@@ -5,21 +5,23 @@
    loops i = 0 < length:
 
        var input = scan // 8
-       var divideCounter = 1
+       var divideCounter = 0
 
        //expect YA
 
-       loops i = 2 < input:
-           if i * i <= input
-               if input % i == 0 && divideCounter > 2 {
-                   print("BUKAN")
-                   break
+       loops j = 1 < input:
+           if j * j <= input
+               if input % j == 0 {
+               	divideCounter++
+				if divideCounter > 1 {
+					break
+				}
                }
-               divideCounter++
-           else {
-               print("YA") // YA
-               break
-           }
+	if divideCounter > 1 {
+		print("BUKAN")
+	}else {
+		print("YA")
+	}
 
 */
 
@@ -35,20 +37,20 @@ func main() {
 		var input int
 		fmt.Scan(&input)
 
-		var divideCounter = 0 // Mulai dari 2 karena setiap bilangan pasti habis dibagi oleh 1 dan bilangan itu sendiri
+		var divideCounter = 0
 
 		for j := 2; j*j <= input; j++ {
 			if input%j == 0 {
+				fmt.Println(j)
 				divideCounter++
-				if divideCounter > 2 { // Jika ada lebih dari dua pembagi, berhenti
+				if divideCounter > 1 {
 					break
 				}
 			}
 		}
-
-		if divideCounter > 2 { // Jika ada lebih dari dua pembagi, maka bukan bilangan agak prima
+		if divideCounter > 1 { // Jika ada lebih dari 2 pembagi, maka bukan bilangan agak prima
 			fmt.Println("BUKAN")
-		} else { // Jika hanya ada dua pembagi, maka bilangan agak prima
+		} else { // Jika hanya ada 2 pembagi, maka bilangan agak prima
 			fmt.Println("YA")
 		}
 	}
